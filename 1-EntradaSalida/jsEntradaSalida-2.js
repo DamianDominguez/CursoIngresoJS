fd52dc99925aaa22c8232810245a7429d28bf4e1
Dominguez Damian Alan <damian_dominguez-12@hotmail.com>
2017-02-01 11:59:45 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,18 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
-{
-	
+
+{	
+ //Esto es un comentario de una linea
+  /* esto es una prueba de progamacion*/
+	var nombre; //Defino Variable
+	nombre= "Damian"; //cargo variable 
+	nombre= prompt("ingrese su nombre","natalia natalia");
+
+
+	alert("nombre:  " +nombre);
+
+	var person = prompt("Ingresa tu Nombre", "natalia natalia");
+
 }
 
