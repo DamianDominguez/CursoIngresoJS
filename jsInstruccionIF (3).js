732f756c87f86ea9107ba34b53806519bc976a9b
Dominguez Damian Alan <damian_dominguez-12@hotmail.com>
2017-02-08 11:14:48 -0300

    Add files via upload

---
+++ jsInstruccionIF (3).js
@@ -0,0 +1,21 @@
+function Mostrar()
+{
+//tomo la edad  
+
+var edad
+
+edad = document.getElementById('edad').value;
+
+if(edad >= 18)
+
+	{
+		alert("Usted es mayor de Edad")
+	}
+
+	if(edad <= 17)
+
+{
+	alert("Usted es menor de Edad")
+}
+		
+}//FIN DE LA FUNCIÓN
