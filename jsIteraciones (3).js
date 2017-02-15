f839d05b9f8916002d8e1902110db96e67105594
Dominguez Damian Alan <damian_dominguez-12@hotmail.com>
2017-02-15 10:20:06 -0300

    Add files via upload

---
+++ jsIteraciones (3).js
@@ -0,0 +1,20 @@
+function Mostrar()
+{
+
+ var clave="cualquier cosa";
+
+
+while(clave!= "utn750")
+
+{
+	var clave = prompt("ingrese el número clave.");
+
+
+	console.log("Bienvenido");
+	
+}
+
+
+
+
+}//FIN DE LA FUNCIÓN
