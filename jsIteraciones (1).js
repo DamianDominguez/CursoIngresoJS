2b8855282dd97b4da79d20bc9a8ce279db7a3f16
Dominguez Damian Alan <damian_dominguez-12@hotmail.com>
2017-02-15 09:43:27 -0300

    Add files via upload

---
+++ jsIteraciones (1).js
@@ -0,0 +1,14 @@
+function Mostrar()
+{
+	
+	var contador = 0;
+
+	while(contador < 10)
+	{
+		document.write(" numero: <br>" + contador + " <br>");
+		contador ++;
+		console.log(contador);
+	}
+
+
+}//FIN DE LA FUNCIÓN
