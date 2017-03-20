19fd705569db199e22908f529cdd9f051ff858d7
Dominguez Damian Alan <damian_dominguez-12@hotmail.com>
2017-03-20 12:18:01 -0300

    Add files via upload

---
+++ ejercicio 1/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+int a;
+int x;
+int media;
+int contador;
+{
+for(contador=0; contador <5; contador++)
+{
+   printf("Ingrese un numero");
+   scanf("%d", &x);
+   a+=x;
+}
+    media =a/5;
+
+    printf("La media de sus numeros es %d")
+
+    return 0;
+}
